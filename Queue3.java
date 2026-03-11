2c4e814151ecfa8f8f75cb44e482c3f9884d896d
renoricardo4206 <rricardo01@student.ciputra.ac.id>
2026-03-11 21:18:42 +0800

    Tugas 6.2 No.3

---
+++ Queue3.java
@@ -0,0 +1,52 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Queue3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        // Validasi constraint: 5 < n < 20
+        if (n <= 5 || n >= 20) {
+            System.out.println("Invalid input: queue length must be between 6 and 19");
+            sc.close();
+            return;
+        }
+
+        String[] names = sc.nextLine().split(" ");
+        String[] chancesStr = sc.nextLine().split(" ");
+
+        // Validasi constraint: 1 <= chances <= 10
+        Queue<Object[]> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            int chance = Integer.parseInt(chancesStr[i]);
+            if (chance < 1 || chance > 10) {
+                System.out.println("Invalid input: chances must be between 1 and 10");
+                sc.close();
+                return;
+            }
+            queue.add(new Object[]{names[i], chance});
+        }
+
+        // Proses queue
+        while (!queue.isEmpty()) {
+            Object[] student = queue.poll();
+            String name = (String) student[0];
+            int chances = (int) student[1];
+
+            chances--;
+
+            if (chances > 0) {
+                System.out.println(name + "|Try Again|" + chances);
+                queue.add(new Object[]{name, chances});
+            } else {
+                System.out.println(name + "|Get Out|" + chances);
+            }
+        }
+
+        sc.close();
+    }
+}
